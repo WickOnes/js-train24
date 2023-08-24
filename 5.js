af26d222c332212fb69c75f4afa25f76f0c1db67
Nikita Lozoviy <105447918+WickOnes@users.noreply.github.com>
2023-08-24 21:48:48 +0300

    finish

--- 5.js
+++ 5.js
@@ -13,6 +13,28 @@ function task5() {
   // Обробляємо помилку, якщо вона виникне
   // Ми використовуємо .finally метод для виконання дій незалежно від того, в якому стані завершився наш проміс, та виводимо повідомлення "Завершення лічильника"
   // Виконуємо код після завершення проміса
+  let counter = 0;
+  const intervalPromise = new Promise((resolve, reject) => {
+    const interval = setInterval(() => {
+      counter += 1;
+      console.log(`Значення лічильника: ${counter}`);
+      if (counter === 5) {
+        clearInterval(interval);
+        resolve(counter);
+      }
+    }, 1000);
+  });
+
+  intervalPromise
+    .then((value) => {
+      console.log(`Значення лічильника: ${value}`);
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+    .finally(() => {
+      console.log("Завершення лічильника");
+    });
 }
 // Викликаємо функцію task5
 task5();
